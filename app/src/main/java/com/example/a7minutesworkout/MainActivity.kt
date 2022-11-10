d5d7aa628be80e8a79313a5929439718d4852977
Rishav Kumar <lit2021028@iiitl.ac.in>
2022-11-10 23:13:10 +0530

    Converted app to use Fragments instead of Activity

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minutesworkout
 
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,53 +9,63 @@ import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.a7minutesworkout.databinding.ActivityMainBinding
+import com.example.a7minutesworkout.databinding.DialogCustomBackConfirmationBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding : ActivityMainBinding? = null
+    private lateinit var navController: NavController
     lateinit var drawerLayout: DrawerLayout
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-//  Drawer Functionality
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+      this.setSupportActionBar(binding!!.toolbarDrawer)
         drawerLayout = binding!!.drawerLayout
         actionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout, R.string.nav_open,R.string.nav_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-       binding?.flStart?.setOnClickListener{
-//            Toast.makeText(this@MainActivity,
-
-           val intent  = Intent(this,ExerciseActivity::class.java)
-           startActivity(intent)
+        if(supportActionBar!=null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-        binding?.flBMI?.setOnClickListener{
-            val intent = Intent(this,BMIActivity::class.java)
-            startActivity(intent)
-        }
-        binding?.history?.setOnClickListener {
-            val intent = Intent(this,HistoryActivity::class.java)
-            startActivity(intent)
-        }
-        binding?.locateGym?.setOnClickListener {
-            val intent = Intent(this, MapActivity::class.java )
-            startActivity(intent)
 
-        }
     }
 
+    override fun onBackPressed() {
+
+//      customBackNavigation()
+        val customDialog = Dialog(this)
+        val dialogBinding = DialogCustomBackConfirmationBinding.inflate(layoutInflater)
+        customDialog.setContentView(dialogBinding.root)
+        customDialog.setCanceledOnTouchOutside(false)
+        dialogBinding.btnYes.setOnClickListener{
+//            this@ExerciseFragment.onDetach()
+            super.onBackPressed()
+            customDialog.dismiss()
+        }
+        dialogBinding.btnNo.setOnClickListener{
+            customDialog.dismiss()
+        }
+        customDialog.show()
+
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if(actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            true
-        }
-        else super.onOptionsItemSelected(item)
-
+    return if(actionBarDrawerToggle.onOptionsItemSelected(item)) {
+        true
     }
+    else super.onOptionsItemSelected(item)
+
+}
+
     override fun onDestroy() {
         super.onDestroy()
         binding = null
     }
+
 }
